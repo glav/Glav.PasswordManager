a14e0ad787aa4ddbebeeea26bd0c68456be36958
Paul Glavich <glav@theglavs.com>
2011-01-16 23:40:25 +1100

    Created and attached Preferences command

---
+++ PasswordMgr/Commands/PreferencesCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace PasswordMgr.Commands
+{
+    class PreferencesCommand : BaseCommand
+    {
+        public override void Execute(object parameter)
+        {
+            MessageBox.Show("Not implemented yet");
+
+        }
+    }
+}
